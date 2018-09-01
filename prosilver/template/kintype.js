01b20e09ff129d825ff14bb228ab163f5430eacb
Lars Paulson <larspcus2@gmail.com>
2018-08-31 23:42:08 -0400

    Add avatar tags

---
+++ prosilver/template/kintype.js
@@ -0,0 +1,15 @@
+var url, avatar;
+$('.AI').each(function() { 
+  url = $(this).attr('avatar');
+  
+  if ($(this).parents(".inner").find("img.avatar").length === 0) {
+    $(this).parents(".inner").find(".avatar-container").append("<img class='avatar'></img>");
+  }
+  avatar = $(this).parents(".inner").find("img.avatar");
+  
+  avatar.css({'max-width': "192px", 'max-height': "150px"});
+  avatar.attr('height', null);
+  avatar.attr('width', null);
+  
+  avatar.attr('src', url);
+});
