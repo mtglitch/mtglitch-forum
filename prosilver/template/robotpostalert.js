a9d1ead6aab13e2307cce207d9b03b2ab89660bf
unknown <okaasan@mtglitch.com>
2019-04-29 23:21:40 -0600

    Increased Update Frequency

--- prosilver/template/robotpostalert.js
+++ prosilver/template/robotpostalert.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var updateFrequency = 2 * 60 * 1000; //two minutes
+  var updateFrequency = 1 * 60 * 1000; //two minutes
   
   window.lastUpdateTime = new Date();
   setInterval(checkNewPosts, updateFrequency);
